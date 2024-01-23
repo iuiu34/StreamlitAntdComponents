efd8ac8b0b0e0744913e8107db5cefba4eaeb1f6
xavier.banos <xavier@boschmoanrt.com>
2024-01-23 22:18:27 +0100

    templates

--- streamlit_antd_components/frontend/src/ts/ColorPicker.tsx
+++ streamlit_antd_components/frontend/src/ts/ColorPicker.tsx
@@ -1,26 +1,18 @@
 import {Streamlit} from "streamlit-component-lib";
 import React, {useEffect, useState} from "react";
-import {ColorPicker, ConfigProvider} from 'antd';
+import {ColorPicker, ColorPickerProps, ConfigProvider} from 'antd';
 import {getTheme} from "../js/utils.react"
-import {BaseProp, LabelWrap} from "./utils";
 import {Color} from "antd/es/color-picker";
 import '../css/color_picker.css'
 
 
-interface ColorPickerProp extends BaseProp {
-    label: any
-    description: any
-}
-
-const AntdColorPicker = (props: ColorPickerProp) => {
+const AntdColorPicker = (props: ColorPickerProps) => {
     //get data
-    const {backgroundColor, size, primaryColor, textColor, theme} = getTheme(props);
-    const label = props['label']
-    const description = props['description']
-    let labelHeight = label !== null ? 64 : 32
+    // const theme = getTheme(props);
+    const labelHeight = 20
     //state
     const [height, setHeight] = useState(labelHeight)
-    const [color, setColor] = useState(props.color);
+    const [value, setColor] = useState(props.value);
 
     // component height
     useEffect(() => Streamlit.setFrameHeight(height))
@@ -32,7 +24,6 @@ const AntdColorPicker = (props: ColorPickerProp) => {
     }
 
     const openChange = (open: boolean) => {
-        let labelHeight = label !== null ? 64 : 32
         setHeight(open ? 270 + labelHeight : labelHeight)
     }
 
@@ -41,22 +32,16 @@ const AntdColorPicker = (props: ColorPickerProp) => {
             theme={{
                 components: {
                     ColorPicker: {
-                        ...theme,
+                        // ...theme,
                     }
                 }
             }}>
-            <LabelWrap
-                label={label}
-                desc={description}
-                grow={true}
-                children={
-                    <ColorPicker
-                        onChange={onChange}
-                        value={color}
-                        defaultValue={primaryColor}
-                        // showText={label}
-                        onOpenChange={openChange}
-                    />}/></ConfigProvider>)
+            <ColorPicker
+                onChange={onChange}
+                value={value}
+                defaultValue={props.defaultValue}
+                onOpenChange={openChange}
+            /></ConfigProvider>)
 };
 
 export default AntdColorPicker
